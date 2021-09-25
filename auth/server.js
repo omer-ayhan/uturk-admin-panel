7e52291e3c5e0070ab0eeb3ea28b96bfb31a8399
Ömer Ayhan <om.ayhan247@gmail.com>
2021-09-25 08:18:08 +0300

    server configurations

--- auth/server.js
+++ auth/server.js
@@ -1,16 +1,8 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 const admin = require("firebase-admin");
 const serviceAccount = require("../firebaseAdminKey.json");
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../build/index.html"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-  });
-}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
