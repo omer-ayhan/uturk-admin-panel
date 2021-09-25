95f870c74e4b9812f30e1bbffe3afc528c6dc7ba
Ömer Ayhan <om.ayhan247@gmail.com>
2021-09-25 09:09:40 +0300

    build confs

---
+++ auth/app.js
@@ -0,0 +1,40 @@
+module.exports = (req, res) => {
+  const admin = require("firebase-admin");
+  const serviceAccount = require("../firebaseAdminKey.json");
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://notetab-8de87.firebaseio.com",
+  });
+
+  let anonNum = 0;
+  return admin
+    .auth()
+    .listUsers(Number(req.query.num) + 1)
+    .then((listUsersResult) => {
+      listUsersResult.users.forEach(function (userRecord) {
+        if (userRecord.providerData.length === 0) {
+          //this user is anonymous
+          console.log(userRecord); // do your delete here
+          ++anonNum;
+          admin
+            .auth()
+            .deleteUser(userRecord.uid)
+            .then(function () {
+              console.log("Successfully deleted user");
+            })
+            .catch(function (error) {
+              console.log("Error deleting user:", error);
+            });
+        }
+      });
+      if (anonNum > 0) {
+        res.send({ users: anonNum, msg: `${anonNum} users deleted` });
+      } else {
+        res.send({ users: 0, msg: "No anonymous users found" });
+      }
+    })
+    .catch(function (error) {
+      res.send({ users: 0, msg: "An error occured" });
+    });
+};
